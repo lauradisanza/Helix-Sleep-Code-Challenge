484b27cfbfdc06f920bd7fd28df5d4f3523717a7
Laura DiSanza <ldisanza7@gmail.com>
2021-09-06 15:25:27 -0400

    Images added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <p>This is a paragraph</p>
+        <?php
+            echo "This is also a paragraph";
+        ?>
+        <p>This is a paragraph!</p>
+
+
+
+
+    </body>
+</html>
