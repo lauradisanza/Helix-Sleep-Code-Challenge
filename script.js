ad291ab9ad8b9afbaf4dd6911ca3fc21b46999dc
Laura DiSanza <ldisanza7@gmail.com>
2021-09-08 02:10:29 -0400

    Nav

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-const mainMenu = document.querySelector('.main-menu');
-const closeMenu = document.querySelector('.close-menu');
-const openMenu = document.querySelector('.open-menu');
-
-//Hamburger menu
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
-function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-
-function close() {
-    mainMenu.style.top = '-100%';
-}
-
-
-
 /* Changing product color */
 
 //Large Images of Variant Colors
